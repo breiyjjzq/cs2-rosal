acea75b39dfc8700c79f3b4c3fc169b48003ac47
breiyjjzq <cpalma2031@wvc.pshs.edu.ph>
2026-09-02 16:42:56 +0800

    Implement distance calculation from user coordinates
    Added user input for coordinates and distance calculation.

--- Palma CB - Rosal (1).py
+++ Palma CB - Rosal (1).py
@@ -1,5 +1,6 @@
 import math
 
+# Ask the user for the coordinates
 point_x1 = float(input("enter x1: "))
 point_x2 = float(input("enter x2: "))
 point_y1 = float(input("enter y1: "))
@@ -9,6 +10,7 @@ point_x = point_x2 - point_x1
 point_y = point_y2 - point_y1
 
 point_xy = pow(point_x, 2) + pow(point_y, 2)
-
+# Compute the distance using sqrt() and pow()
 distance = math.sqrt(point_xy)
+# Display the result
 print("The distance is", distance)
